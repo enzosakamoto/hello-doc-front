2d49753ceec733f91f6b0c8e8b3dbb7ea0fa0d12
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-26 19:27:50 -0300

    feat: add navbar component with theme toggle

---
+++ src/components/navbar.tsx
@@ -0,0 +1,49 @@
+import { Moon, Sun } from 'lucide-react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
+import { Button } from '@/components/ui/button'
+import { useTheme } from './theme-provider'
+
+export function Navbar() {
+  const { setTheme } = useTheme()
+
+  return (
+    <nav className="fixed left-0 top-0 z-50 flex w-full border-b-[1px] border-gray-300 p-2 px-12 font-geist">
+      <div className="absolute left-0 top-0 -z-10 h-full w-full bg-background/30 backdrop-blur-md"></div>
+      <div className="flex w-full items-center justify-between">
+        <a
+          href="/"
+          className="scroll-m-20 text-xl font-extrabold tracking-tight lg:text-xl"
+        >
+          Hello Doc
+        </a>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme('light')}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('dark')}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('system')}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  )
+}
