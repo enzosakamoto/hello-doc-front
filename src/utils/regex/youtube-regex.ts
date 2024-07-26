2be7ad9e75fc59113c2ae5e990bdbc28c44a0adb
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-26 19:28:34 -0300

    feat: add youtube regex utility for validating YouTube URLs

---
+++ src/utils/regex/youtube-regex.ts
@@ -0,0 +1,3 @@
+export const youtubeRegex =
+  // eslint-disable-next-line no-useless-escape
+  /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
